4dd4bcc1558762f150959606016e8246170266d7
kandalog <asuasu.tee@gmail.com>
2022-08-25 20:08:00 +0900

    スキーマを用意してDBと接続

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 25,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 50,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    coverPicture: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    city: {
+      type: String,
+      max: 50,
+    },
+  },
+  { timestamps: trues }
+);
--- server.js
+++ server.js
@@ -3,12 +3,23 @@ const app = express();
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postsRoute = require("./routes/posts");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+// DB接続
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("DBと接続を開始します。");
+  })
+  .catch((err) => console.log(err));
+
+// API
 app.get("/", (req, res) => {
   res.send("hello express");
 });
 
-// ミドルウェア
+// ミドルウェア(module化したAPI)
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postsRoute);
